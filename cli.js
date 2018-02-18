764d57bb32c6e4cc64faf6a5a75b46452e28e6be
Roberto Thais <r@robertothais.org>
2018-02-17 19:39:43 -0500

    No need for "ok" and "fail" to be functions

--- cli.js
+++ cli.js
@@ -6,6 +6,8 @@ const fs = require("fs");
 const ohm = require("ohm-js");
 
 const command = process.argv[2];
+const ok = "[ \u001b[32m OK \u001b[0m ]";
+const fail = "[ \u001b[31m FAIL \u001b[0m ]\n";
 const usage = `Ohm is parser generator.
 
 Usage:
@@ -19,7 +21,7 @@ function check(src) {
   try {
     return ohm.grammar(src);
   } catch (err) {
-    console.log(fail());
+    console.log(fail);
     console.log(err.message);
     process.exit(1);
   }
@@ -30,7 +32,7 @@ function parse(grammar, input) {
   if (match.succeeded()) {
     return grammar.trace(input);
   } else {
-    console.log(fail());
+    console.log(fail);
     console.log(match.message);
     process.exit(1);
   }
@@ -44,24 +46,16 @@ function read(filepath) {
   return fs.readFileSync(path.resolve(process.cwd(), filepath), "utf8");
 }
 
-function ok() {
-  return "[ \u001b[32m OK \u001b[0m ]";
-}
-
-function fail() {
-  return "[ \u001b[31m FAIL \u001b[0m ]\n";
-}
-
 switch (command) {
   case "check": {
     check(read(process.argv[3], command));
-    console.log(ok());
+    console.log(ok);
     break;
   }
   case "parse": {
     const grammar = check(read(process.argv[3], command));
     const trace = parse(grammar, read(process.argv[4], command));
-    console.log(ok());
+    console.log(ok);
     if (process.argv[5] === "--trace") {
       console.log("\n" + trace.toString());
     }
